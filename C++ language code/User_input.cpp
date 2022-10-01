335849486adf471f09765e2172d8f35b88fbf2e3
itxmayankjain <114828742+itxmayankjain@users.noreply.github.com>
2022-10-01 23:50:12 +0530

    program

---
+++ C++ language code/User_input.cpp
@@ -0,0 +1,37 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter the value a"  <<endl;
+    cin>>a;
+    if(a>0){
+        cout<<"the value is postive";
+    }
+    else{
+    if(a<0){
+        cout<<"the value is negative";
+    }
+    else{
+        cout<<"value is zero";
+    }
+    }
+
+}*/
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    if(ch>='A' && ch<='Z'){
+        cout<<"the capital alphabat"<<endl;
+    }
+    else if(ch>='a' && ch<='z'){
+        cout<<"ch is small alphabat"<<endl;
+    }
+    else if( ch>='0' && ch<='9'){
+        cout<<"this ch is a numeric"<<endl;
+    }
+
+}*/
+
---
+++ C++ language code/whileLoop.cpp
@@ -0,0 +1,25 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        cout<<" "<<i;
+        i++;
+    }
+}*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int sum = 0;
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        sum = sum+i;
+       cout<<sum;
+       
+    }
+}
