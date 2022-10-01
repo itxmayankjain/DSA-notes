70e0ccc34616aa6206cd0b97447616bf56ccadf1
itxmayankjain <114828742+itxmayankjain@users.noreply.github.com>
2022-10-02 00:18:45 +0530

    abc

--- C++ language code/whileLoop.cpp
+++ C++ language code/whileLoop.cpp
@@ -13,13 +13,13 @@ int main(){
 #include<iostream>
 using namespace std;
 int main(){
-    int sum = 0;
     int n;
     cin>>n;
-    int i = 2;
+    int sum = 0;
+    int i = 1;
     while(i<=n){
-        sum = sum+i;
-       cout<<sum;
-       
+       sum = sum+i;
+       i= i+1;
     }
+    cout<<sum;
 }
